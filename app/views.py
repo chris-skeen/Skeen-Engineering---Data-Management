7a4157e26ebc554c7c70e73e2361b464873f43a3
Chris Skeen <chrisskeen2006@gmail.com>
2025-03-24 08:46:40 -0500

    Revert "updated admin panel, figuring out admin template for main pages."
    This reverts commit c20a415038d18a064a864e7136e1eef65ec7fd8e.

--- app/views.py
+++ app/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 
 # hey
-
-def home_view(request):
-  
-  return render(request, "home.html", {})
--- config/settings.py
+++ config/settings.py
@@ -31,14 +31,12 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "app"
 ]
 
 MIDDLEWARE = [
@@ -123,7 +121,3 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-UNFOLD = {
-  "SITE_HEADER": "Skeen Engineering"
-}
